603b37d97f5bc2016d4d74b5d9fafcd390f176aa
laibaiqbal552 <laibaiqbal552>
2022-12-23 23:34:07 +0500

    .

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 place-content-center place-items-center xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2">
+    <div className="grid grid-cols-1 gap-4 place-content-center place-items-center xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3">
       {products.map(({ id, logo, title }) => {
         return (
           <div key="id" className="p-5 bg-white rounded-sm shadow-lg md:mx-0">
